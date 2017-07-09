14fae87e6c27f2a83bded0e8e89cc83ed3848592
Sebastijan Horvat <sebastijan.horvatt@gmail.com>
2017-07-09 13:22:37 +0200

    dodan izbornik.js
    trenutno za canvas animacije

---
+++ glavni_izbornik/izbornik.js
@@ -0,0 +1,272 @@
+$(document).ready(function(){
+	
+	var kut = 0; //kasnije ide još *Math.PI	
+	var smjer = 1;
+	var animVjesala = setInterval(crtajAnimaciju, 100);
+	var animKrizic = setInterval(crtajKrizic,500);
+	var animUpitnik = setInterval(crtajUpitnik, 1000);
+	var animStaza = setInterval(crtajStazu, 100);
+	
+
+	var brojPolja = 0;
+	var naRedu = 1;
+	var ispunjena = new Array();
+	var bojePolja = new Array();
+	var boja1 = "rgb(0,0,255)";
+	var boja2 = "rgb(255,0,0)";
+
+	var slova = ['A','B','C','D'];
+
+	
+
+	function crtajTruplo()
+	{
+		var ctx = $("#vjesala").get(0).getContext("2d");
+
+		ctx.save();
+		ctx.lineWidth = 4;
+		ctx.scale(0.5,0.5); ctx.translate(35,0);
+		ctx.beginPath();
+		ctx.moveTo(10,350); ctx.lineTo(110,350); //baza
+		ctx.moveTo(60,350); ctx.lineTo(60,50); //štap
+		ctx.lineTo(210,50);//greda
+
+		ctx.translate(210,50);
+		ctx.rotate(kut);
+		
+		ctx.moveTo(0,0);
+		ctx.lineTo(0,40); //kuka
+		ctx.translate(0,40);
+		ctx.moveTo(0,0); 
+		ctx.arc(0,30,30,1.5*Math.PI,3.5*Math.PI); //glava
+		ctx.translate(0,60);
+		ctx.moveTo(0,0); ctx.lineTo(0,100); //truplo
+		ctx.translate(0,30);
+		//nadalje desno misli se na moje desno, slično za lijevo
+		ctx.moveTo(0,0); ctx.lineTo(50,-50); //desna ruka
+		ctx.moveTo(0,0); ctx.lineTo(-50,-50); //lijeva ruka
+		ctx.translate(0,70);
+		ctx.moveTo(0,0); ctx.lineTo(50,50); //desna noga
+		ctx.moveTo(0,0); ctx.lineTo(-50,50); //lijeva noga
+		ctx.stroke();
+		ctx.restore();
+		
+	}
+
+	function crtajAnimaciju()
+	{
+		if( kut > 0.05*Math.PI ) smjer = -1;
+		else if( kut < -0.05*Math.PI ) smjer = 1;
+		kut += smjer*0.005*Math.PI;
+
+		var h = $("#vjesala").height();
+		var w = $("#vjesala").width();
+		var ctx = $("#vjesala").get(0).getContext("2d");
+		ctx.clearRect(0,0,w,h);
+
+		crtajTruplo();
+	}
+
+	function crtajKrizic()
+	{
+		//isprazni canvas
+		var ctx=$("#krizic").get(0).getContext("2d");
+		var w = $("#krizic").width();
+		var h = $("#krizic").height();
+		ctx.clearRect(0,0,w,h);
+	
+		//treba li crtati iznova?
+		if(brojPolja === 9)
+		{
+			brojPolja = 0;
+			ispunjena = new Array();
+			bojePolja = new Array();
+			boja1 = "rgb("+Math.floor(Math.random()*256)
+				+","+Math.floor(Math.random()*256)
+				+","+Math.floor(Math.random()*256)+")";
+			boja2 = "rgb("+Math.floor(Math.random()*256)
+				+","+Math.floor(Math.random()*256)
+				+","+Math.floor(Math.random()*256)+")";
+		}
+
+		//odaberi neobojano random polje od 0 do 8
+		var kojePolje = Math.floor(Math.random()*9);
+		while( ispunjena.indexOf(kojePolje) !== -1 )
+			kojePolje = Math.floor(Math.random()*9);
+		ispunjena.push(kojePolje);
+
+
+		//koja je boja za polje
+		if( naRedu === 1 )
+			bojePolja.push(boja1);
+		else
+			bojePolja.push(boja2);
+
+		//oboji polja do sad ispunjena
+		for( var i = 0; i < ispunjena.length; ++i)
+		{
+			var redak = Math.floor(ispunjena[i]/3);
+			var stupac = ispunjena[i]%3;
+			ctx.fillStyle = bojePolja[i];
+			ctx.fillRect(Math.floor(redak*h/3),Math.floor(stupac*w/3),Math.floor(w/3),Math.floor(h/3));
+		}
+
+		//iscrtaj vodoravne i okomite crte
+		ctx.lineWidth = 3;
+		ctx.beginPath();
+		
+		ctx.moveTo(0,Math.floor(h/3));
+		ctx.lineTo(w,Math.floor(h/3));
+		ctx.moveTo(0,Math.floor(2*h/3));
+		ctx.lineTo(w,Math.floor(2*h/3));
+
+		ctx.moveTo(Math.floor(w/3),0);
+		ctx.lineTo(Math.floor(w/3),h);
+		ctx.moveTo(Math.floor(2*w/3),0);
+		ctx.lineTo(Math.floor(2*w/3),h);
+
+		ctx.stroke();
+
+		//promijeni stanje sustava
+		++brojPolja;
+		naRedu = 1 - naRedu;	
+	}
+
+	function crtajUpitnik()
+	{
+		//isprazni canvas
+		var ctx=$("#upitnik").get(0).getContext("2d");
+		var w = $("#upitnik").width();
+		var h = $("#upitnik").height();
+		ctx.clearRect(0,0,w,h);
+
+		ctx.font= Math.floor(h/5)+"px Arial";
+		ctx.textAlign = "center";
+		ctx.lineWidth = 3;
+		var odabran = Math.floor(Math.random()*4);
+
+		for(var i = 0; i < 4; ++i)
+		{
+			if( i === odabran )
+				ctx.fillStyle = "green";
+			else
+				ctx.fillStyle = "black";
+
+			ctx.fillText(slova[i],Math.floor(3*w/4),Math.floor((i+1)*h/4-h/24));
+
+			if( i === odabran )
+			{	
+				ctx.fillRect(Math.floor(w/4),Math.floor(i*h/4+h/24),Math.floor(w/5),Math.floor(h/6));
+			}
+			ctx.strokeRect(Math.floor(w/4),Math.floor(i*h/4+h/24),Math.floor(w/5),Math.floor(h/6));
+				
+			//crtaj upitnik kod odabranog odgovora
+			if( i === odabran )
+			{
+				ctx.fillStyle = "red";
+				ctx.fillText("?",Math.floor(w/8),Math.floor((i+1)*h/4-h/24));
+			}
+		}
+	
+	}
+
+	var tockex = new Array();
+	var tockey = new Array();
+	var boje = new Array();
+	var novax = 0;
+	var novay = 0;
+	var smjerx = 1;
+	var smjery = 1;
+	var begin = 0;
+
+	function predznak(varijabla)
+	{
+		if(varijabla > 0)
+			return 1;
+		else return -1;
+	}
+
+	function crtajStazu()
+	{
+		//isprazni canvas
+		var ctx=$("#staza").get(0).getContext("2d");
+		var w = $("#staza").width();
+		var h = $("#staza").height();
+		ctx.clearRect(0,0,w,h);
+	
+		if( tockex.length >= 10 || begin === 0)
+		{
+			begin = 1;
+			tockex = new Array();
+			tockey = new Array();
+			boje = new Array();
+			if(Math.floor(Math.random()*2) === 1)
+				smjerx = Math.floor(Math.random()*5+2);
+			else
+				smjerx = - Math.floor(Math.random()*5+2);
+			if(Math.floor(Math.random()*2) === 1)
+				smjery = Math.floor(Math.random()*5+2);
+			else
+				smjery = - Math.floor(Math.random()*5+2);
+
+			var randomBoja = "rgb("+Math.floor(Math.random()*256)
+				+","+Math.floor(Math.random()*256)
+				+","+Math.floor(Math.random()*256)+")";
+			boje.push(randomBoja);
+
+			novax = Math.floor(Math.random()*(w/4)+w/4);
+			novay = Math.floor(Math.random()*(h/4)+h/4);
+			tockex.push(novax);
+			tockey.push(novay);
+		}
+		
+		ctx.lineWidth = 5;		
+
+		var i = 0;
+		for( i = 0; i < tockex.length-1; ++i )
+		{
+			ctx.beginPath();
+			ctx.strokeStyle = boje[i];
+			ctx.moveTo(tockex[i],tockey[i]);
+			ctx.lineTo(tockex[i+1],tockey[i+1]);
+			ctx.stroke();
+		}
+		
+		ctx.beginPath();
+		ctx.strokeStyle = boje[i];
+		ctx.moveTo(tockex[i],tockey[i]);
+
+		if( novax+smjerx >= w || novax+smjerx <= 0 ) 
+		{
+			smjerx = (-1)*predznak(smjerx)*Math.floor(Math.random()*5+2);
+			smjery = predznak(smjery)*Math.floor(Math.random()*5+2);
+			var randomBoja = "rgb("+Math.floor(Math.random()*256)
+				+","+Math.floor(Math.random()*256)
+				+","+Math.floor(Math.random()*256)+")";
+			boje.push(randomBoja);
+			tockex.push(novax);
+			tockey.push(novay);
+
+		}
+		if( novay+smjery >= h || novay+smjery <= 0 ) 
+		{
+			smjerx = predznak(smjerx)*Math.floor(Math.random()*5+2);
+			smjery = (-1)*predznak(smjery)*Math.floor(Math.random()*5+2);
+			var randomBoja = "rgb("+Math.floor(Math.random()*256)
+				+","+Math.floor(Math.random()*256)
+				+","+Math.floor(Math.random()*256);
+			boje.push(randomBoja);
+			tockex.push(novax);
+			tockey.push(novay);
+		}
+		
+		novax += smjerx;
+		novay += smjery;
+
+		ctx.lineTo(novax,novay);
+
+		ctx.stroke();
+
+	}
+
+});	
