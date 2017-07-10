6e8e9c742e9ced025d1b29fa1c89f2fba215ecc0
Sebastijan Horvat <sebastijan.horvatt@gmail.com>
2017-07-10 17:25:11 +0200

    detaljno komentiran kod
    dodani komentari u kod

--- utils/login.js
+++ utils/login.js
@@ -1,6 +1,10 @@
 $(document).ready(function()
 { 
+	//ova varijabla je samo da nešto šaljem, uglavnom za testiranje
 	var dummy = null;
+	
+	//kontaktiram login.php i između dvije crte(hr) ispisujem što već vrati
+	//dakle, formu za login, ili formu za logout, ... (za detalje vidi taj php)
 	$.ajax(
 	{
 		url: "../utils/login.php",
@@ -20,6 +24,9 @@ $(document).ready(function()
 		async: false
 	});
 
+
+	//klik na gumb za login, kontaktira se ponovo login.php koji radi provjere itd.
+	//je li dobar username, pass, ...
 	$("body").on("click","#loginklasa1",function(){
 		var usr = $("#username").val();
 		var pas = $("#password").val();
@@ -41,8 +48,10 @@ $(document).ready(function()
 		} );
 	});
 
+	
 	var logout = true;	
 
+	//klikom na logout, kontaktira se login.php koji radi logout
 	$("body").on("click","#loginklasa2",function(){
 		$.ajax(
 		{
@@ -64,13 +73,17 @@ $(document).ready(function()
 		$("#profil").css("display","none");
 	});
 
+	//dodaj u div sa id-jem pocetni naslovnu traku sa canvasom u koji ćemo crtati
 	var divElement = $("#pocetni");
 	
-	divElement.prepend('<canvas height="100" width="100" id="naslovniCanvas">Nije podrzan canvas</canvas>');
+	divElement.prepend('<canvas height="100" width="100" '
+			+ 'id="naslovniCanvas">Nije podrzan canvas</canvas>');
 
+	//na početku crtaj naslovnu, i onda mijenjaj svake sekunde(vidi kod te funkcije)
 	crtajNaslovnu();
 	setInterval(crtajNaslovnu, 1000);
 
+	//ovo crta digitalni prikaz broja danih dimenzija i na danim koord. i boje
 	function crtajBroj(broj,koordx,koordy,dimenzija,boja)
 	{
 		var ctx = $("#naslovniCanvas").get(0).getContext("2d");
@@ -80,7 +93,7 @@ $(document).ready(function()
 		ctx.strokeStyle = boja;
 
 		ctx.beginPath();
-		switch(broj) {
+		switch(broj) { //u ovisnosti koji je broj - tako ga crtaj
     		case 0:
         		ctx.strokeRect(koordx,koordy,dimenzija,dimenzija);
         		break;
@@ -142,6 +155,9 @@ $(document).ready(function()
 
 	}
 
+	//u naslovni canvas crtaj uz fju gore digitalni sat - znamenku po znamenku
+	//sati su crvene boje, minute plave, a sekunde zelene
+	//gdje se crtaju i njihove dimenzije su namještene u ovisnosi o dim canvasa
 	function crtajVrijeme(sat,minuta,sekunda)
 	{
 		var ctx = $("#naslovniCanvas").get(0).getContext("2d");
@@ -152,16 +168,25 @@ $(document).ready(function()
 		if(Math.floor(h/2) < Math.floor(w/16)) dimenzija = Math.floor(h/2);
 		else dimenzija = Math.floor(w/16);
 
-		crtajBroj(Math.floor(sat/10),w-7*dimenzija-30,Math.floor(h/2),dimenzija,"red");
-		crtajBroj(sat%10,w-6*dimenzija-25,Math.floor(h/2),dimenzija,"red");
+		crtajBroj( Math.floor(sat/10) , w-7*dimenzija-30 ,
+			Math.floor(h/2) , dimenzija, " red " );
+		crtajBroj( sat%10 , w-6*dimenzija-25 , 
+			Math.floor(h/2) , dimenzija , "red" );
 
-		crtajBroj(Math.floor(minuta/10),w-5*dimenzija-20,Math.floor(h/2),dimenzija,"blue");
-		crtajBroj(minuta%10,w-4*dimenzija-15,Math.floor(h/2),dimenzija,"blue");
+		crtajBroj( Math.floor(minuta/10) , w-5*dimenzija-20 ,
+			Math.floor(h/2) , dimenzija , "blue" );
+		crtajBroj( minuta%10 , w-4*dimenzija-15 , 
+			Math.floor(h/2) , dimenzija , "blue" );
 
-		crtajBroj(Math.floor(sekunda/10),w-3*dimenzija-10,Math.floor(h/2),dimenzija,"green");
-		crtajBroj(sekunda%10,w-2*dimenzija-5,Math.floor(h/2),dimenzija,"green");
+		crtajBroj( Math.floor(sekunda/10) , w-3*dimenzija-10 , 
+			Math.floor(h/2) , dimenzija , "green");
+		crtajBroj( sekunda%10 , w-2*dimenzija-5 , 
+			Math.floor(h/2) , dimenzija , "green" );
 	}
 
+	//crta naslovnu traku - naslov (boja je random pa se mijenja svakim pozivom)
+	//te se dohvaća datum i vrijeme i uz ovo gore crta sat
+	//Napomena: bio je i prikaz datuma, ali je bila prevelika gužva na canvasu!
 	function crtajNaslovnu()
 	{
 		var width = window.innerWidth;
@@ -192,28 +217,39 @@ $(document).ready(function()
 		crtajVrijeme(datetime.getHours(),datetime.getMinutes(),datetime.getSeconds());
 	}
 
-
+	//funkcija daje random boju u rgb formatu
 	function randomBoja() {
   	var boja = 'rgb('+Math.floor(Math.random()*256)+','+Math.floor(Math.random()*256)+','+Math.floor(Math.random()*256)+')';
   	return boja;
 	}
 
-	var element = '<p id="podaci" style="display:inline"> Created by: ';
-	element += '<b text="red"> isschl </b>';
-	element += ' Povratak: ';
-	element += '<a href="..">';
-	element += '<img src="../utils/data/homeButton.jpg"';
-	element += ' id="returnPicture"';
-	element += ' alt="HOME" border="0" />';
-	element += '</a></p>';
+	//sljedeće će ispuniti div sa id-jem zavrsni - svojevsni footer za stranicu
+	//unutra skraćenice naših imena, slika kao link na početnu stranicu,
+	//te se dodaje audio tag koji svira neku pjesmu iz data foldera
 
+	var element = '<p id="podaci" style="display:inline"> Created by: '
+		+ '<b text="red"> isschl </b>'
+		+ ' Povratak: '
+		+ '<a href="..">'
+		+ '<img src="../utils/data/homeButton.jpg"'
+		+ ' id="returnPicture"'
+		+ ' alt="HOME" border="0" />'
+		+ '</a></p>';
+
+	//primjetiti da je izbor pjesme koja je na autoplayu i u loop-u random!
 	$("#zavrsni")
 	.html(element)
 	.css("font-size","120%")
-	.append('<audio controls autoplay loop style="display:inline" ><source src="../utils/data/Song'+Math.floor(Math.random()*3+1)+'.mp3" type="audio/ogg">Your browser does not support the audio element.</audio>');
+	.append('<audio controls autoplay loop style="display:inline" >'
+		+ '<source src="../utils/data/Song'
+		+ Math.floor(Math.random()*3+1)
+		+ '.mp3" type="audio/ogg">Your browser does not support the'
+		+ ' audio element.</audio>');
 
+	//na početku se zove funkcija namjesti(za detalje vidi njen kod)
 	namjesti();
 
+	//namješta se slika iz footera i sam footer opisan iznad
 	function namjesti()
 	{
 		$("#zavrsni")	
@@ -224,9 +260,11 @@ $(document).ready(function()
 		.css("width",Math.floor(($(window).width())*0.05));
 	}
 
+	//uz resize ide novo namještanje(kod je iznad)
 	$( window ).resize(namjesti);
 
-
+	//klikom na gumb id-ja mojprofil, pokazuje se profil za igrača
+	//a to je ona tablica koju stvara dajProfil.php (za detalje vidi taj kod)
 	$("body").on("click","#mojprofil",pokaziProfil);
 
 
@@ -281,7 +319,7 @@ $(document).ready(function()
 		});
 	}
 
-
+	//zatvaranje profila na njegov gumb za zatvaranje
 	$( "body" ).on( "click", "div.profil #closeProfil", function()
 	{
 		$("#profil").css("display","none");
