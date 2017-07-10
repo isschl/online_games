e8a19875f44f1ba621b168d1d675cae16d414ce5
Sebastijan Horvat <sebastijan.horvatt@gmail.com>
2017-07-10 16:18:46 +0200

    komentiran kod
    kod komentiran i uređen

--- utils/upute.js
+++ utils/upute.js
@@ -1,5 +1,8 @@
 $(document).ready(function()
 {
+	//klikom na npr. div sa id-jem tekstuputa, njegov sadržaj se skriva
+	//te se dodaje u div ispred ostalog sadržaja (nešto kao gallery iz dz)
+ 
 	function stvori_zaprikaz( prozor )
 	{
 		$( "body" ).prepend( prozor );
@@ -22,9 +25,11 @@ $(document).ready(function()
 		.css( "bottom", "0" );
 	}
 
+	//dodaj tom stvorenom prozoru gumb kojim se zatvara
 	function dodaj_izlazgumb()
 	{
-	$( "#prozor" ) . prepend( $('<button id="close" class = "galer" >X</button>') );
+	$( "#prozor" ) . prepend( $('<button id="close" '
+				+ 'class = "galer" >X</button>') );
 	$( "#close" ) . css("float","right");
 	$( "#close" ) . css( "background-color", "#f44336" );
 	$( "#close" ).css("padding",$("button.galer").width());
@@ -37,31 +42,49 @@ $(document).ready(function()
 		.css( "height",  "40px" );
 	}
 
+	//varijabla govori je li prozor prikazan - na dnu imamo da se na resize
+	//prilagođava dimenzijama - ali samo ako je prikazan!
+
 	var enabled = 0;	
+
+
 
 	function pokaziProzor()
 	{
+	//prozor je prikazan, pa je enabled postavljen na 1
 	enabled = 1;
+
+	//ako je bio neki start prozor, ukloni ga
 	$("#prozor").remove();
 	var prozor = $('<div id="prozor" class="prozor"></div>');
 	stvori_zaprikaz( prozor );
+
+	//dodaj mu gumb za izlaz
 	dodaj_izlazgumb();
 
-	$("#prozor").append('<div style="color:white; position:fixed; padding:2em; left:50%; top:50%; transform:translate(-50%, -50%);" id="dodano">'+$("#tekstuputa").html()+'</div>');
+	$("#prozor").append('<div style="color:white; position:fixed; '
+		+ 'padding:2em; left:50%; top:50%; '
+		+ 'transform:translate(-50%, -50%);" id="dodano">'
+		+ $("#tekstuputa").html()+'</div>');
 
 	$("#prozor").css( "display" , "block" );
 	}
 
+	//makni tekst na početku koji će se prikazati klikom na tag id-ja upute
 	$("#tekstuputa") . css( "display" , "none" );
 
 	$("#upute").on("click",pokaziProzor);
 
+	//zatvori prozor ako je kliknuto na gumb za izlazak iz njega
 	$( "body" ).on( "click", "div.prozor #close", function()
 	{
 			enabled = 0;
 			$("#prozor").css("display","none"); 
 	});
 
+
+
+	//na resize, pokaži ponovo taj prozor s prilagođenim dimenzijama
 	$( window ).resize(function() {
   		if(enabled === 1) pokaziProzor();
 	});
